49b3649f4deb714cb1f59c06a6788f42d70f650b
Eric Clark <eclark0013@gmail.com>
2019-08-06 14:40:48 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,10 @@
 INSERT INTO bears (name, age) VALUES
-  ("Mr. Chocolate", 5);
+  ("Mr. Chocolate", 5),
+  ("Rowdy", 5),
+  ("Tabitha", 12),
+  ("Sergeant Brown", 15),
+  ("Melissa", 16),
+  ("Grinch", 9),
+  ("Wendy", 14),
+  (NULL, 22)
+  ;
